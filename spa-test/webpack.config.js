97d85127456ab9566913541cf90abe28b862283a
junzzi <jjh01027717502@gmail.com>
2022-03-28 17:58:54 +0900

    release: spa-test netlify 설정 추가

--- spa-test/webpack.config.js
+++ spa-test/webpack.config.js
@@ -1,7 +1,7 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-
+const CopyPlugin = require("copy-webpack-plugin");
 module.exports = {
   mode: "development",
   entry: "./src/index.js",
@@ -16,6 +16,7 @@ module.exports = {
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
+    publicPath: "/",
   },
   module: {
     rules: [
@@ -37,6 +38,9 @@ module.exports = {
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       template: "./index.html",
+    }),
+    new CopyPlugin({
+      patterns: [{ from: "public", to: "" }],
     }),
   ],
 };
